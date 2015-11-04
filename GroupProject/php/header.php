f9805ec9b3029d235f9283546af46cf7f995923e
doppelgangerhef <austinhoefs@gmail.com>
2015-11-04 16:19:08 -0600

    changed header links

--- GroupProject/php/header.php
+++ GroupProject/php/header.php
@@ -12,9 +12,13 @@
 	<div id="navbar" class="collapse navbar-collapse">
 		<ul class="nav navbar-nav">
 		<li class="active"><a href="#">Home</a></li>
-		<li><a href="#about">About</a></li>
-		<li><a href="#contact">Contact</a></li>
-		<li class="dropdown">
+		<li><a href="articles.html">Articles</a></li>
+		<li><a href="services.html">Services</a></li>
+		<li><a href="clients.html">Clients</a></li>
+		<li><a href="services.html">Staff</a></li>
+		<li><a href="contact.html">Contact Us</a></li>
+		<li><a href="portal.html">Client Portal</a></li>
+		<!--<li class="dropdown">
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 			<ul class="dropdown-menu">
 			<li><a href="#">Action</a></li>
@@ -26,6 +30,7 @@
 			<li><a href="#">One more separated link</a></li>
 			</ul>
 		</li>
+		-->
 		</ul>
 	</div>
 	</div>
