cca35c33b29ebb8e0cd4e8e0af0621e8bdc96c4d
likwam29 <likwam29@uwosh.edu>
2015-11-08 16:56:20 -0600

    Added a template and sidebar to the php folder.

---
+++ GroupProject/php/template.php
@@ -0,0 +1,56 @@
+
+<!--
+	This is a template. That means you do not dev you stuff here.
+	you just copy this into a new file and start from there.
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<!--<link rel="icon" href="../../favicon.ico">-->
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="css/site.css" rel="stylesheet">
+        <title>Change me</title>
+    </head>
+    <body>
+    
+		<!-- Header Include -->
+		<?php include "php/header.php"; ?>
+		
+		<!-- Begin page content -->
+		<div class="container">
+		    <div class="row">
+			
+				<!-- sidebar Include -->
+				<?php include "php/sidebar.php";?> 
+				
+			    <div class="col-md-9">
+					<h2 id="sec0">Change Me!!!</h2>
+					<!--
+					
+						
+						Your stuff goes here
+					
+					
+					-->
+				</div> 
+			</div>
+		</div>
+
+		<!-- Footer Include -->
+		<?php include "php/footer.php"; ?>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
