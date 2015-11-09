9de7f38a98425c053cb379e2de963193e2747cdf
hoefsa97 <hoefsa97@rlad.housing.uwosh.edu>
2015-11-08 18:10:18 -0600

    renamed files, finished services

---
+++ GroupProject/services.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<!--<link rel="icon" href="../../favicon.ico">-->
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+
+		<!-- Custom styles for this template -->
+		<link href="css/site.css" rel="stylesheet">
+        <title>Services</title>
+    </head>
+    <body>
+    
+		<!-- Header Include -->
+		<?php include "php/header.php"; ?>
+		
+		<!-- Begin page content -->
+		<div class="container">
+		    <div class="row">
+			
+				<!-- sidebar Include -->
+				<?php include "php/sidebar.php";?> 
+				
+			    <div class="col-md-9">
+					<h2 id="sec0">Training</h2>
+					<article>
+						<h3><a href="services/mediation.php">Mediation</a></h3>
+						<p>
+							Mediation is a voluntary process in which parties choose to work together with an impartial third party (mediator) to resolve their conflict in a mutually beneficial way.  
+							It is a confidential, uniquely flexible process that can be tailored to meet a wide variety of situations.
+						</p>
+						<ul>
+							<li>Workplace</li>
+							<li>Business</li>
+							<li>Family</li>
+							<li>Special Education</li>
+							<li>Environmental</li>
+							<li>Community</li>
+							<li>Civil</li>
+							<li>Churches</li>
+						</ul>
+					</article>
+					<article>
+						<h3><a href="services/facilitation.php">Facilitation Services</a></h3>
+						<p>
+							A process in which an impartial third party (facilitator) works with a group by helping group members communicate and problem-solve productively, resulting in high quality decisions.  
+							The facilitator provides structure so the group can address the content.
+						</p>
+						<ul>
+							<li>Consensus Building</li>
+							<li>Problem-Solving</li>
+						</ul>
+					</article>
+					<article>
+						<h3><a href="services/consultation.php">Consultation</a></h3>
+						<p>
+							In order to effectively address organizational needs, an organization must first understand the scope of the need. 
+							A consultant with specific expertise works with a client to identify the problem, recommend possible solutions/services, and identify available resources.  
+							The objectivity of the consultant provides valuable insight.  
+							By partnering with the client, the client is actively engaged in addressing organizational issues and conflicts, with the support of the consultant's expertise.  
+							The most frequent needs of organizations fall into these categories.
+						</p>
+						<ul>
+							<li>Needs Assessment</li>
+							<li>Team Building</li>
+							<li>Facilitating Change</li>
+							<li>Focus Groups</li>
+						</ul>
+					</article>
+					<article>
+						<h3><a href="services/coaching.php">Coaching</a></h3>
+						<p>
+							Coaching is a learning partnership for generating creative, purposeful action toward one person's goals and desires.  
+							It is an effective means to help people improve job performance, build leadership skills, and remove obstacles blocking their path to success.
+						</p>
+						<ul>
+							<li>Team</li>
+							<li>Executive</li>
+							<li>Individual</li>
+						</ul>
+					</article>
+					<article>
+						<h3><a href="services/training.php">Training</a></h3>
+						<p>
+							Mediation is a voluntary process in which parties choose to work together with an impartial third party (mediator) to resolve their conflict in a mutually beneficial way.  
+							It is a confidential, uniquely flexible process that can be tailored to meet a wide variety of situations.
+						</p>
+						<ul>
+							<li>Mediation</li>
+							<li>Constructive Communication Skills</li>
+							<li>Communication Styles</li>
+							<li>Conflict Management</li>
+							<li>Change Management</li>
+							<li>Gender Communication</li>
+							<li>Teamwork/Teambuilding</li>
+							<li>Meeting Management</li>
+							<li>Workplace Violence Prevention</li>
+							<li>Collaborative Problem-Solving</li>
+						</ul>
+					</article>
+					<article>
+						<h3>Systems Design</h3>
+						<ul>
+							<li>ACE - Addressing Conflict Effectively</li>
+							<li>Custom design of conflict management systems</li>
+						</ul>
+					</article>
+				</div> 
+			</div>
+		</div>
+
+		<!-- Footer Include -->
+		<?php include "php/footer.php"; ?>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
