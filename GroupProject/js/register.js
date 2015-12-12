2a8941fdf121fe8888a501ab428623dc32bdd66a
grayk58 <grayk58@uwosh.edu>
2015-12-12 14:10:44 -0600

    finally finished the is staff register

--- GroupProject/js/register.js
+++ GroupProject/js/register.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
     var registerForm = $('#registerForm');
-
+	var staffVeriForm = $("#staffVeriForm");
+	var isValidStaffLogin = false;
     $("#registerForm input").on("blur", function() {
     	if(registerForm.hasClass("dirty")){
     		validateForm("registerForm");
@@ -17,21 +18,65 @@ $(document).ready( function() {
 		if(!valid) { 
 			return;
 		}
-		
-		$.ajax( {
-			type: "POST",
-			url: "php/controller/registerController.php",
-			data: registerForm.serialize(),
-			dataType: "json",
-			success: function(response) {
-				console.log(response.success);
-				if(response.success){
-					window.location = "clientPortal.php";
-				} else {
-					alert('An error has occured! Error in console.');
-					console.log(response.errorMessage);
-				}
-			}
-		});  
+		var isStaff = $("input[name=isStaff]:checked").val()
+		if(isStaff == 1){
+			$("#staffVeriModal").modal("show");
+			var staffForm = $("#staffVeriForm");
+			//this will have username and password
+			$("#staffVeriSubmit").on("click", function(){
+				//this is staffVeriController
+				$.ajax( {
+					type: "POST",
+					url: "php/controller/staffVeriController.php",
+					data: staffVeriForm.serialize(),
+					dataType: "json",
+					success: function(response) {
+						if(response.success){
+							console.log("successful");
+							isValidStaffLogin = true;
+						}else if(!response.success){
+							console.log("not successful");
+							isValidStaffLogin = false;	
+						} else {
+							alert('An error has occured! Error in console.');
+							console.log(response.errorMessage);
+						}
+					}
+				}).done(function(){
+					isVerifed();
+				}); 
+			})
+		}else{
+			registerUser();
+		}			
 	});
+	//functions that might make things easier
+	var isVerifed = function(){
+		if(!isValidStaffLogin){
+			$("#responseMessage").empty().append("Invalid Staff Login");
+		}else{
+			$("#responseMessage").empty().append("Welcome, Staff");
+			registerUser();
+			
+		}
+	};
+	var registerUser =  function(){
+		$.ajax( {
+				type: "POST",
+				url: "php/controller/registerController.php",
+				data: registerForm.serialize(),
+				dataType: "json",
+				success: function(response) {
+					console.log(response.success);
+					if(response.success){
+						window.location = "clientPortal.php";
+					} else {
+						alert('An error has occured! Error in console.');
+						console.log(response.errorMessage);
+					}
+				}
+			}); 
+	};
 });
+
+			
--- GroupProject/php/controller/staffVeriController.php
+++ GroupProject/php/controller/staffVeriController.php
@@ -1,23 +1,21 @@
 <?php 
 	header('Content-Type', 'appplication/json');
 	session_start();
-	$username = $_POST["userName"];
-	$password = $_POST["password"];
+	$username = $_POST["staffUser"];
+	$password = $_POST["staffPass"];
 	include_once"../dbConfig.php";
 	
 	$loginResponse = array("success" => FALSE);
 	try{
-		$query = $db->prepare("SELECT Id FROM users WHERE(email = ? AND $password = ?) AND is_staff = 1");
+		$query = $db->prepare("SELECT Id FROM users WHERE(email = ? AND password = ?) AND is_staff = 1");
 		$query->execute(array($username, $password));
 		$rows = $query->fetchALL(PDO::FETCH_ASSOC);
 		if($query->rowCount() == 1){
-			$_SESSION['userInformation'] = $rows;
 			$loginResponse['success'] = TRUE;
-			$loginResponse['isStaff'] = $rows[0]['is_staff'];
 		}
 	}catch(PDOException $ex){
 		echo "An Error occured!";
 		echo $ex->getMessage();
 	}
-	echo json_encode(loginResponse);
+	echo json_encode($loginResponse);
 ?>
